8dddc05d6c63e4d0099c6493bd953feaa36cdd3d
romaintailhurat <romain.tailhurat@gmail.com>
2018-03-29 10:56:36 +0200

    Dynamic server url depending on env var

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,18 @@ import { Client } from "boardgame.io/react";
 import { GameBoard } from "./GameBoard";
 import CrystalHunt from "../Game";
 
-console.log(process.env.server_url);
+const url =
+  process.env.NODE_ENV === "development"
+    ? "localhost:7000"
+    : "crystal-hunt.appspot.com";
+
+console.log("target url", url);
 
 const App = Client({
   game: CrystalHunt,
   board: GameBoard,
   multiplayer: {
-    server: "localhost:7000"
-    // server: "crystal-hunt.appspot.com"
+    server: url
   }
 });
 
