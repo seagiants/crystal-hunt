0bdee2c2d46ee4caa9c4d5ee07f9430d23705c53
bibirico <bibirico@yahoo.fr>
2018-04-16 12:17:06 +0200

    Add randomized decks

--- src/cards/Card.ts
+++ src/cards/Card.ts
@@ -19,6 +19,24 @@ export function loadUpgrade(card: Card): Card {
   return UpgradeLib[card.upgradeName!];
 }
 
+// Using Fisher–Yates Shuffle algorithm (https://bost.ocks.org/mike/shuffle/)
+// TODO ?: Could be implemented in a non-mutating way
+export function shuffle<T>(array: T[]): T[] {
+  var m = array.length,
+    t,
+    i;
+  // While there remain elements to shuffle…
+  while (m) {
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+
 export function loadCards(skillCategory: SkillCategoryName): Array<Card> {
   const test = Object.keys(CardLib).reduce(
     (temp, prop) =>
@@ -27,7 +45,7 @@ export function loadCards(skillCategory: SkillCategoryName): Array<Card> {
         : temp,
     []
   );
-  return test;
+  return shuffle(test);
 }
 
 export function loadDecks(): Decks {
