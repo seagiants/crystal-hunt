6c6e9cf92156359b38f3742bfe1dd855e8094871
romaintailhurat <romain.tailhurat@gmail.com>
2018-04-27 09:16:37 +0200

    A bigger map, still some issues with dimensions

--- src/map/mapDefinitions.ts
+++ src/map/mapDefinitions.ts
@@ -24,51 +24,13 @@ export const initCell = (cellType: CellTypeName): Cell => {
   };
 };
 
-/*
-export const basicMap: MapDef = {
-  xMax: 2,
-  yMax: 2,
-  cells: {
-    "0x0": { ...initCell(CellTypeName.RoomCell), avatar: "0" },
-    "1x0": { ...initCell(CellTypeName.RoomCell), avatar: "M2" },
-    "1x1": initCell(CellTypeName.BlackCrystalCell),
-    "1x2": initCell(CellTypeName.RoomCell),
-    "2x2": { ...initCell(CellTypeName.RoomCell), avatar: "1" }
-  },
-  blackCrystalCellXY: "1x1"
-};
-
-const _longerMap: MapDef = {
-  xMax: 6,
-  yMax: 4,
-  blackCrystalCellXY: "3x2",
-  cells: {
-    "0x0": { ...initCell(CellTypeName.RoomCell), avatar: "0" },
-    "1x0": initCell(CellTypeName.RoomCell),
-    "1x1": initCell(CellTypeName.RoomCell),
-    "1x2": initCell(CellTypeName.RoomCell),
-    "2x0": { ...initCell(CellTypeName.RoomCell), avatar: "M2" },
-    "3x0": initCell(CellTypeName.RoomCell),
-    "3x1": { ...initCell(CellTypeName.RoomCell), trap: true },
-    "3x2": initCell(CellTypeName.BlackCrystalCell),
-    "3x3": { ...initCell(CellTypeName.RoomCell), trap: true },
-    "3x4": initCell(CellTypeName.RoomCell),
-    "4x4": { ...initCell(CellTypeName.RoomCell), avatar: "M3" },
-    "5x4": initCell(CellTypeName.RoomCell),
-    "5x3": initCell(CellTypeName.RoomCell),
-    "5x2": initCell(CellTypeName.RoomCell),
-    "6x4": { ...initCell(CellTypeName.RoomCell), avatar: "1" }
-  }
-};
-*/
-
 // prettier-ignore
 const firstMapStruct = [
-  ["R0-", "R--", "R--"],
-  ["---", "R--", "R--"],
-  ["R--", "B--", "R--"],
-  ["R--", "R--", "---"],
-  ["R--", "R--", "R1-"]
+  ["R0-", "R--", "R--", "R--", "R--", "R--", "R--"],
+  ["R--", "---", "---", "R--", "---", "---", "---"],
+  ["R--", "R--", "R--", "B--", "R--", "R--", "R--"],
+  ["---", "---", "---", "R--", "---", "---", "R--"],
+  ["R--", "R--", "R--", "R--", "R--", "R--", "R1-"]
 ];
 
 // TODO : Improve BlackCrsytalCell handle.
